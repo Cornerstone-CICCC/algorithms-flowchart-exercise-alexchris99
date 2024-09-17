7d76b9037e8ab728c6086fa25ba8759f25af493b
Christian Jimenez <cajt@outlook.com>
2024-09-17 12:10:58 -0800

    Final Version

--- js/exercise_5.js
+++ js/exercise_5.js
@@ -4,11 +4,11 @@ let ma = "Math";
 let mark = 60;
 
 if(mark >= 80){
-    console.log(`${ma}, Grade A`);
+    console.log(`${ma}%, Grade A`);
 }else if(mark >= 60 && mark < 80 ){
-    console.log(`${ma}, Grade B`);
+    console.log(`${ma}%, Grade B`);
 }else if(mark >= 40 && mark < 60){
-    console.log(`${ma}, Grade C`);
+    console.log(`${ma}%, Grade C`);
 }else{
-    console.log(`${ma},No grade`);
+    console.log(`${ma}%,No grade`);
 }
