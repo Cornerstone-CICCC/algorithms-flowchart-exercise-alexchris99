3b5555b65a3af3b1f0e869679fa2da9fc6ad4a1d
Christian Jimenez <cajt@outlook.com>
2024-09-17 12:21:10 -0800

    Exercise 5 final

--- js/exercise_5.js
+++ js/exercise_5.js
@@ -4,11 +4,11 @@ let subjet = "Math";
 let mark = 60;
 
 if(mark >= 80){
-    console.log(`${subjet}%, Grade A`);
+    console.log(`${subjet} ${mark}%, Grade A`);
 }else if(mark >= 60 && mark < 80 ){
-    console.log(`${subjet}%, Grade B`);
+    console.log(`${subjet} ${mark}%, Grade B`);
 }else if(mark >= 40 && mark < 60){
-    console.log(`${subjet}%, Grade C`);
+    console.log(`${subjet} ${mark}%, Grade C`);
 }else{
-    console.log(`${subjet}%,No grade`);
+    console.log(`${subjet} ${mark}%,No grade`);
 }
